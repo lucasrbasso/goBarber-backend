d4bd30145e0d878402e1d93a91678364c2e13abe
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-14 15:58:35 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333');
+});
