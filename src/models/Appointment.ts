2becb21abecfaf4f5851ad3cfb8c1069f5d56a23
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-17 16:58:04 -0300

    Integrating Postgres

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,27 @@
-import { uuid } from 'uuidv4';
+/* eslint-disable camelcase */
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+} from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     provider: string;
 
+    @Column('timestamp with time zone')
     date: Date;
 
-    constructor({ provider, date }: Omit<Appointment, 'id'>) {
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
+    @CreateDateColumn()
+    created_at: Date;
+
+    @CreateDateColumn()
+    updated_at: Date;
 }
 
 export default Appointment;
