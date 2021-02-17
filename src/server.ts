8ddd1bcbd876af68ed66f1da75e53b8bedd69736
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-17 16:58:13 -0300

    Update server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 /* eslint-disable no-console */
+import 'reflect-metadata';
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
