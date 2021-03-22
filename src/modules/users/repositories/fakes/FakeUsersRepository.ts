68a48b4903f94439e2f9eb861b7b174bc6ec6a79
Lucas Basso <lucas.rbasso@outlook.com>
2021-03-22 15:33:02 -0300

    update name of class

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -4,7 +4,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../../infra/typeorm/entities/User';
 
-class UsersRepository implements IUserRepository {
+class FakeUsersRepository implements IUserRepository {
     private users: User[] = [];
 
     public async findById(id: string): Promise<User | undefined> {
@@ -40,4 +40,4 @@ class UsersRepository implements IUserRepository {
     }
 }
 
-export default UsersRepository;
+export default FakeUsersRepository;
