0b78c8416f214d1030216525574587b62bb962bd
Lucas Basso <lucas.rbasso@outlook.com>
2021-03-26 16:28:23 -0300

    appointments services: list all providers except by id now working

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+
+export default class ProfileController {
+    public async index(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const listProvidersService = container.resolve(ListProvidersService);
+        const user_id = request.user.id;
+
+        const providers = await listProvidersService.execute({ user_id });
+
+        return response.json(providers);
+    }
+}
