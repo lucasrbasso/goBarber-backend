3ea9ff7045b1b843fe502f4dc46226a89a991ca8
Lucas Basso <lucas.rbasso@outlook.com>
2021-03-26 20:38:04 -0300

    appointments repository: new function findAllInMonth()

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDto from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProvider from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
     create(data: ICreateAppointmentDto): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
+    findAllInMonthFromProvider(
+        data: IFindAllInMonthFromProvider,
+    ): Promise<Appointment[]>;
 }
